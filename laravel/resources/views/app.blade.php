243bdce654a02fa90ff07d490ba49dff7bacb8cc
Zac Batog <batogz@mymacewan.ca>
2015-03-26 22:52:34 -0400

    puts logout button back in

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -6,42 +6,40 @@
         <link rel="stylesheet" href="/css/app.css" type="text/css"/>
         <div class="quote">Edmonton Public Library</div>
     </head>
-
     <body>
-    	<nav class="navbar navbar-default">
-    		<div class="container-fluid">
-    			<div class="navbar-header">
-    				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-    					<span class="sr-only">Toggle Navigation</span>
-    					<span class="icon-bar"></span>
-    					<span class="icon-bar"></span>
-    					<span class="icon-bar"></span>
-    				</button>
-    				<a class="navbar-brand" href="/welcome">Home</a>
-    			</div>
-    			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    				<ul class="nav navbar-nav">
-    					<li><a href="/bookings">Bookings</a></li>
-    					<li><a href="/kits">Kits</a></li>
-    					<li><a href="/admin">Admin</a></li>
-    				</ul>
-    				<ul class="nav navbar-nav navbar-right">
-    					@if (Auth::guest())
-				    		<li><a href="/auth/login">Login</a></li>
-				    		<li><a href="/auth/register">Register</a></li>
-			    		@else
-				    		<li class="dropdown">
-				    			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-				    			<ul class="dropdown-menu" role="menu">
-				    				<li><a href="/auth/logout">Logout</a></li>
-				    			</ul>
-				    		</li>
+	    <nav class="navbar navbar-default">
+	    	<div class="container-fluid">
+	    		<div class="navbar-header">
+	    			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	    				<span class="sr-only">Toggle Navigation</span>
+	    				<span class="icon-bar"></span>
+	    				<span class="icon-bar"></span>
+	    				<span class="icon-bar"></span>
+	    			</button>
+	    			<a class="navbar-brand" href="/welcome">Home</a>
+	    		</div>
+	    		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	    			<ul class="nav navbar-nav">
+	    				<li><a href="/bookings">Bookings</a></li>
+	    				<li><a href="/kits">Kits</a></li>
+	    				<li><a href="/transfers">Transfers</a></li>
+	    			</ul>
+	    			<ul class="nav navbar-nav navbar-right">
+	    				@if (Auth::guest())
+	    					<li><a href="/auth/login">Login</a></li>
+	    					<li><a href="/auth/register">Register</a></li>
+	    				@else
+	    					<li class="dropdown">
+	    						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+	    						<ul class="dropdown-menu" role="menu">
+	   							<li><a href="/auth/logout">Logout</a></li>
+		    					</ul>
+		    				</li>
 		    			@endif
-			    	</ul>
+		    		</ul>
 		    	</div>
-	    	</div>
-	    </nav>
-	    
-	    @yield('content')
+		    </div>
+    	</nav>
+    	@yield('content')
 
     </body>
